471abe3df6e52a1f46f68317177ead4bc3a4b0b2
cracycot <cracycot@mail.ru>
2025-01-07 23:26:08 +0300

    feat: updated dependencies

---
+++ src/main/java/org/example/chatrealtimeservice/ChatRealTimeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.chatrealtimeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRealTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRealTimeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/chatrealtimeservice/ChatRealTimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatrealtimeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRealTimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
